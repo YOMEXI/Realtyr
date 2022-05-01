f652430664603ade307d8bc33e73317d789b6cbf
YOMEXI <abayomexy@gmail.com>
2022-05-01 04:35:51 +0100

    cleaned

--- utils/fetchApi.tsx
+++ utils/fetchApi.tsx
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-// headers: {
-//     'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
-//     'X-RapidAPI-Key': 'b9cb32d90dmshab05d04d379ff7bp1e4fe1jsne04c4c2b5e8d'
-//   }
-
 export const baseURL = "https://bayut.p.rapidapi.com";
 
 export const fetchApi = async (url: string) => {
